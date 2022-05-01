5a5706ffbca65fcbe1b2d1a22e6c578498e37633
soroushbahmani <soroush.bahmani.ir@gmail.com>
2022-05-01 18:30:45 +0430

    update coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -7,9 +7,9 @@ const Coin = ({name,image,symple,price,marketCap,priceChange}) => {
         <img className={style.image} src={image} alt={'icon'+name}/>
         <span className={style.name}>{name}</span>
         <span className={style.symple}>{symple.toUpperCase()}</span>
-        <span className={style.currentPrice}>{price.toLocaleString()}</span>
-        <span className={priceChange > 0 ? style.greenPrice : style.redPrice}>{priceChange}</span>
-        <span className={style.marketCap}>{marketCap.toLocaleString()}</span>
+        <span className={style.currentPrice}> $ {price.toLocaleString()}</span>
+        <span className={priceChange > 0 ? style.greenPrice : style.redPrice}>{priceChange.toFixed(2)}</span>
+        <span className={style.marketCap} > ${marketCap.toLocaleString()}</span>
     </div>
   )
 }
