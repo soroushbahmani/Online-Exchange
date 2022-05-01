1cea2a559cab48524c592604d8973d903c8d07af
soroushbahmani <soroush.bahmani.ir@gmail.com>
2022-05-01 19:47:56 +0430

    add select tag for show row and add input for pageination

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -12,48 +12,76 @@ const Landing = () => {
 
     const [coins, setCoin] = useState([]);
     const [search, setSearch] = useState("");
+    const [perr, setPer] = useState(5);
+    const [page, setPage] = useState(1);
+
+
 
     useEffect(() => {
         const fetchApi = async () => {
-            const data = await getCoin();
+            const data = await getCoin(perr, page);
             setCoin(data)
+            // console.log(data.length);
         }
         fetchApi()
-    }, [])
+    }, [perr, page])
 
     const searchHandler = event => {
         setSearch(event.target.value)
     }
     const searchCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
 
+    const pageinationClick = (event) => {
+        setPer(event.target.value)
+    }
+    const pageHandler = event => {
+        setPage(event.target.value)
+    }
     return (
         <>
+            <div className={style.topInputs}>
+                <div>
+                    <span>row</span>
+                    <select className={style.select} value={perr} onChange={pageinationClick}>
+                        <option value='1' >1</option>
+                        <option value='5' >5</option>
+                        <option value='10'>10</option>
+                        <option value='25' >25</option>
+                        <option value='50'>50</option>
+                    </select>
+                </div>
+                <div>
+                    <span>page</span>
+                    <input className={style.input0} type='number' placeholder='pageination' min={1} onChange={pageHandler} value={page} />
+                </div>
+            </div>
             <input className={style.input1} type='text' placeholder='search' value={search} onChange={searchHandler} />
 
-                {
-                    coins.length ?
-                        <div className={style.coinContainer}>
-                            {
-                                searchCoins.map(item => <Coin
-                                    key={item.id}
-                                    name={item.name}
-                                    image={item.image}
-                                    symple={item.symbol}
-                                    price={item.current_price}
-                                    marketCap={item.market_cap}
-                                    priceChange={item.price_change_percentage_24h}
-                                />)
-                            }
+
+            {
+                coins.length ?
+                    <div className={style.coinContainer}>
+                        {
+                            searchCoins.map(item => <Coin
+                                key={item.id}
+                                name={item.name}
+                                image={item.image}
+                                symple={item.symbol}
+                                price={item.current_price}
+                                marketCap={item.market_cap}
+                                priceChange={item.price_change_percentage_24h}
+                            />)
+                        }
 
 
-                        </div> :
-                        <Loader type={'loading'}/>
-                }
-                {
-                    search.length ? 
-                    <Loader type={'not'}/>:
+                    </div> :
+                    <Loader type={'loading'} />
+            }
+            {
+                search.length ?
+                    <Loader type={'not'} /> :
                     ''
-                }
+            }
 
         </>
     )
